291036a361c061ee4f6c1657041ede18083ee55a
Abhishek Yadav <abhi7566.ay@gmail.com>
2022-04-16 19:03:49 +0530

    javascript let & var

---
+++ javascript let, var/script.js
@@ -0,0 +1,78 @@
+//VAR
+// var g = "Hello World";
+// var g = "Indore";
+
+// console.log(g);
+
+// var x = "India";
+
+// function goa(){
+//     var y ="Bye";                // local variable
+
+//     console.log(y);
+
+//     console.log(x);
+// }
+// goa();
+
+// console.log(y);
+
+// // function indore(){
+//     a = "Hello Abhishek";
+
+//     console.log(a);
+//     var h = "Welcome To Goa";
+//     console.log(h);
+// }
+// indore();
+// console.log(a);
+
+// var a = "Hi This Is JavaScript";
+// var time = 5;
+
+// if(time > 4){
+//     var a = "Hello User";
+// }
+
+// console.log(a);
+
+//LET
+
+// let canot be redeclare
+// blocked scope
+
+// let d = "Hi Abhishek";
+// let d = "Bye Abhishek";
+ 
+// console.log(d);
+
+// let r = 10;
+// let s = 20;
+// {
+//     let r = 100;
+//     console.log(r);
+// }
+
+// r = 40;
+// console.log(r);
+
+// let hello;
+// consolele.log(hello);
+
+// var hi;
+// console.log(hi);
+
+
+// JSON -->  JAVASCRIPT  OBJECT NOTATION
+
+let Abhishek = {
+    name: "Abhishek Yadav",
+    age:  "21",
+    college: "Medicaps",
+}
+console.log(Abhishek);
+console.log(Abhishek.name);
+console.log(Abhishek.age);
+console.log(Abhishek.college);
+console.log(Abhishek['name']);
+console.log(Abhishek['age']);
